d1c1fd7f8fe71a9a728d6b3d6efb36dd234c75ea
julianyap2 <yjulian74@gmail.com>
2021-11-28 18:34:10 +0700

    lanjut ke router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home/Home.view';
-import Gallery from './components/pages/Gallery/Gallery.view';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home.view";
+import Gallery from "./components/pages/Gallery/Gallery.view";
 function App() {
   return (
     <Router>
-      <Home/>
-      <Gallery/>
+      <Home />
+      <Routes>
+        <Route path='/' component={<Home/>} />
+        <Route path='/Gallery' component={<Gallery/>} />
+      </Routes>
     </Router>
   );
 }

3033630062844b4d29520d64dbe1c654b82374d3
julianyap2 <yjulian74@gmail.com>
2021-12-05 17:49:31 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Switch } from "react-router-dom";
 import Home from "./components/pages/Home/Home.view";
 import Gallery from "./components/pages/Gallery/Gallery.view";
 function App() {
   return (
     <Router>
       <Home />
-      <Routes>
+      <Switch>
         <Route path='/' component={<Home/>} />
         <Route path='/Gallery' component={<Gallery/>} />
-      </Routes>
+      </Switch>
     </Router>
   );
 }
--- src/components/Navbar/Navbar.view.js
+++ src/components/Navbar/Navbar.view.js
@@ -1,81 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from '../Button/Button.view.js';
-import { Link } from 'react-router-dom';
-import './Navbar.styled.css';
-import logo from '../../KOPANA-BANDUNG.png';
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+  Logo
+} from './Navbar.styled';
+import image from '../../images/logo.svg'
 
-function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
-
+const Navbar = () => {
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            <img src={logo} alt="Logo" className='logo'/>
-            <i class='fab fa-typo3' />
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Beranda
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/Gallery'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Galeri
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/about'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Peminjaman
-              </Link>
-            </li>
-
-            <li>
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Log In
-              </Link>
-            </li>
-          </ul>
-          {button && <Button buttonStyle='btn--outline'>Log In</Button>}
-        </div>
-      </nav>
+      <Nav>
+        <NavLink to='/'>
+            <img src={image} alt={'Logo'} style={{height:"70px"}}/>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/beranda' activeStyle>
+            Beranda
+          </NavLink>
+          <NavLink to='/gallery' activeStyle>
+            Galeri
+          </NavLink>
+          <NavLink to='/peminjaman' activeStyle>
+            Peminjaman
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
     </>
   );
-}
+};
 
 export default Navbar;

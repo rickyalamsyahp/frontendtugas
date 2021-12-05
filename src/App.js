2021cf47e13244bc8b7e4183a184ac49ffa07175
julianyap2 <yjulian74@gmail.com>
2021-12-05 23:33:10 +0700

    router v6 done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
 import Home from "./components/pages/Home/Home.view";
 import Gallery from "./components/pages/Gallery/Gallery.view";
+import Peminjaman from "./components/pages/Peminjaman/Peminjaman.view"
 function App() {
   return (
     <Router>
-      <Home />
-      <Switch>
-        <Route path='/' component={<Home/>} />
-        <Route path='/Gallery' component={<Gallery/>} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/gallery" element={<Gallery />} />
+        <Route exact path="/beranda" element={<Home />} />
+        <Route exact path="/peminjaman" element={<Peminjaman />} />
+
+
+      </Routes>
     </Router>
-  );
+    
+);
 }
 
 export default App;
